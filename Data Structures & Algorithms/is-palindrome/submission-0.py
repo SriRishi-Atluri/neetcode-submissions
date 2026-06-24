2f05cbcf1495ebd5dfc3895451c52a3f2c5c8c7d
SriRishi Atluri <145803190+SriRishi-Atluri@users.noreply.github.com>
2026-06-24 14:27:58 -0400

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        fixed_string = s.lower()
+        palindrome_string = ""
+
+        for c in fixed_string:
+            if c.isalnum():
+                palindrome_string += c
+
+        return palindrome_string == palindrome_string[::-1]
+        
